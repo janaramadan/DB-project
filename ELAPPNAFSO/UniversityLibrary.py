3f7235224cf0491d64a42ec92e85ce787407509e
Mohamed Khamis <52880248+mohamed20045@users.noreply.github.com>
2024-05-18 19:46:20 +0300

    Added Update Functions

--- ELAPPNAFSO/UniversityLibrary.py
+++ ELAPPNAFSO/UniversityLibrary.py
@@ -33,6 +33,20 @@ insertBook(cursor, 9789876543210, '2003-11-07', 'BOOK DESCRIPTION', 200, 'BOOK T
 
 
 
+# 2 UPDATE FUNCTIONS
+def updateAdminEmail(cursor, admin_id, new_email):
+    cursor.execute(
+        "UPDATE Admin SET Email = ? WHERE Admin_ID = ?",
+        (new_email, admin_id)
+    )
+
+def updateBookDescription(cursor, isbn, new_description):
+    cursor.execute(
+        "UPDATE Book SET Description = ? WHERE ISBN = ?",
+        (new_description, isbn)
+    )
+
+
 
 def getAllStudents(cursor):
     cursor.execute("SELECT * FROM Student")
