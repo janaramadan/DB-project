cefaad234c1b584a59da9d3d79db7a34165483ae
faridayoussry <faridayoussry13@gmail.com>
2024-05-18 22:13:06 +0300

    Updated SELECT Statements

--- ELAPPNAFSO/UniversityLibrary.py
+++ ELAPPNAFSO/UniversityLibrary.py
@@ -27,11 +27,23 @@ def insertBook(cursor, isbn, publish_year, description, pages, title):
         (isbn, publish_year, description, pages, title)
     )
 
+def insertAdmin(cursor, admin_id, password, username, email, country, city, street):
+    cursor.excute(
+        "INSERT INTO Admin (Admin_ID, Password, Username, Email, Country, City, Street) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (admin_id, password, username, email, country, city, street)
+    )
+
+def insertBorrowedBooks(Borrowed_ID, Student_ID, ISBN):
+    cursor.excute(
+        "INSERT INTO BorrowedBook (Borrowed_ID, Student_ID, ISBN) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (Borrowed_ID, Student_ID, ISBN)
+    )
+
 # GARAB BA
 insertStudent(cursor, 3, '123', 'Jana', 'Jana@gmail.com', 'EGYPT', 'Cairo', '123 STREET')
 insertBook(cursor, 9789876543210, '2003-11-07', 'BOOK DESCRIPTION', 200, 'BOOK TITLE')
-
-
+insertAdmin(cursor, 2, '456', 'farida' , 'farida@gmail.com', 'EGYPT', 'Cairo', '456 STREET')
+insertBorrowedBooks(cursor, 4, 3, 9789876543210)
 
 # 2 UPDATE FUNCTIONS
 def updateAdminEmail(cursor, admin_id, new_email):
@@ -45,7 +57,7 @@ def updateBookDescription(cursor, isbn, new_description):
         "UPDATE Book SET Description = ? WHERE ISBN = ?",
         (new_description, isbn)
     )
-    
+
 
 def getAllStudents(cursor):
     cursor.execute("SELECT * FROM Student")
